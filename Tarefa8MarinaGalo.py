21593e95773b1008e3b4c737b2bf13abc04be783
Marina Galo <51497313+batistagalo@users.noreply.github.com>
2019-07-12 20:47:15 -0300

    Add files via upload

---
+++ Tarefa8MarinaGalo.py
@@ -0,0 +1,56 @@
+'''
+    Tarefa 8 - BCC
+    
+    '''
+    
+# Faça um programa que mostre se o número inteiro guardado na variável x é par ou ímpar
+
+# Caso os números a, b, c e d seja pares vão aprensentar resto igual a 0
+# E caso sejam ímpar vão apresentar o resto diferente de 0
+# Por isso foi utilizado % (resto) 
+    
+    
+# Para a variável (a igual a 1
+a= 1
+y= a%2
+
+if y == 0:
+    print('a é par =', a)
+else:
+    print('a é ímpar =', a)
+
+
+    
+    
+# Para a variável (b) igual a 24
+b= 24
+y= b%2    
+    
+if y == 0:
+    print('b é par =', b)
+else:
+    print('b é ímpar =', b)
+
+
+    
+
+# Para a variável (c) igual a 10
+c= 10
+y= c%2
+
+if y == 0:
+    print('c é par =', c)
+else:
+    print('c é ímpar =', c)
+    
+
+    
+    
+# Para a variável (d) igul a 5
+d= 5
+y= d%2
+if y == 0:
+    print('d é par =', d)
+else:
+    print('d é ímpar =', d) 
+    
